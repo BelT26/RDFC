ab5d0fdaa05ac66052b97831b071f46021f1a9a5
BelT26 <helen.taylor@hotmail.it>
2021-12-17 10:55:13 +0000

    add view, url and template for members.html

--- club/views.py
+++ club/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Member
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'club/index.html')
+
+
+def members(request):
+    return render(request, 'club/members.html')

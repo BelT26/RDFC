4678a078b4d65c8aba4f52857d08b9a237d18699
BelT26 <helen.taylor@hotmail.it>
2021-12-17 10:13:50 +0000

    add player model

--- club/models.py
+++ club/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    last_name = models.CharField(max_length=50, null=False, blank=False)
+    username = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=200, null=False, blank=False)
+    password = models.CharField(max_length=50, null=False, blank=False)
+    played = models.IntegerField(default=0)
+    won = models.IntegerField(default=0)
+    drawn = models.IntegerField(default=0)
+    lost = models.IntegerField(default=0)
+    points = models.IntegerField(default=0)
+    is_approved = models.BooleanField(default=False)
+    is_available = models.BooleanField(default=False)
+    is_manager = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"

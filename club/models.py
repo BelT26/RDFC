1e19f0556029549725dd90a1e198a7f32736a257
BelT26 <helen.taylor@hotmail.it>
2021-12-19 11:31:56 +0000

    add Team and Match models

--- club/models.py
+++ club/models.py
@@ -19,3 +19,16 @@ class Member(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Team(models.Model):
+    colour = models.CharField(max_length=10)
+    match_date = models.DateField()
+    players = models.ManyToManyField(Member)
+
+
+class Match(models.Model):
+    match_date = models.DateTimeField()
+    teams = models.ManyToManyField(Team)
+    blue_goals = models.IntegerField(default=0)
+    white_goals = models.IntegerField(default=0)

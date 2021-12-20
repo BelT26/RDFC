1ce857fa96d60a610a72dbe732b03b42e3a32c4a
BelT26 <helen.taylor@hotmail.it>
2021-12-20 10:07:05 +0000

    add tests to check for last_name, username and email to TestSignUpForm

--- club/test_forms.py
+++ club/test_forms.py
@@ -6,10 +6,64 @@ from .forms import SignUpForm
 
 class TestSignUpForm(TestCase):
     def test_first_name_is_required(self):
-        form = SignUpForm({'first_name': ''})
+        form = SignUpForm({
+            'first_name': '',
+            'last_name': 'Smith',
+            'email': 'harry@gmail.com',
+            'username': 'HarryS',
+            'password': 'me'
+        })
         self.assertFalse(form.is_valid())
         self.assertIn('first_name', form.errors.keys())
         self.assertEqual(form.errors['first_name'][0], 'This field is required.')
+
+    def test_last_name_is_required(self):
+        form = SignUpForm({
+            'first_name': 'Harry',
+            'last_name': '',
+            'email': 'harry@gmail.com',
+            'username': 'HarryS',
+            'password': 'me'
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('last_name', form.errors.keys())
+        self.assertEqual(form.errors['last_name'][0], 'This field is required.')
+
+    def test_email_is_required(self):
+        form = SignUpForm({
+            'first_name': 'Harry',
+            'last_name': 'Smith',
+            'email': '',
+            'username': 'HarryS',
+            'password': 'me'
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('email', form.errors.keys())
+        self.assertEqual(form.errors['email'][0], 'This field is required.')
+    
+    def test_username_is_required(self):
+        form = SignUpForm({
+            'first_name': 'Harry',
+            'last_name': 'Smith',
+            'email': 'harry@gmail.com',
+            'username': '',
+            'password': 'me'
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('username', form.errors.keys())
+        self.assertEqual(form.errors['username'][0], 'This field is required.')
+
+    def test_password_is_required(self):
+        form = SignUpForm({
+            'first_name': 'Harry',
+            'last_name': 'Smith',
+            'email': 'harry@gmail.com',
+            'username': 'HarryS',
+            'password': ''
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('password', form.errors.keys())
+        self.assertEqual(form.errors['password'][0], 'This field is required.')
 
     def test_password_must_be_at_least_8_characters(self):
         form = SignUpForm({

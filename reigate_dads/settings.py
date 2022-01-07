0c1ac8cf0305a1fdebd769bc08fa580a003c2bb7
BelT26 <helen.taylor@hotmail.it>
2022-01-07 15:04:02 +0000

    add django messages to settings.py and base.html

--- reigate_dads/settings.py
+++ reigate_dads/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 import dj_database_url
 if os.path.isfile("env.py"):
     import env
@@ -57,6 +58,16 @@ SITE_ID = 1
 LOGIN_REDIRECT = '/'
 LOGOUT_REDIRECT = '/'
 
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -90,6 +101,8 @@ TEMPLATES = [
         },
     },
 ]
+
+
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`

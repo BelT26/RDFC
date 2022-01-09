709e1bc64634e0c05d12b2338c0ca5f5c60e5c03
BelT26 <helen.taylor@hotmail.it>
2022-01-09 12:09:14 +0000

    add if block and further images to members.html

--- club/urls.py
+++ club/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('social', views.social, name='social'),
     path('signup', views.mysignup, name='signup'),
     path('thankyou', views.thankyou, name='thankyou'),
-    path('login', views.mylogin, name='login'),
+    #path('login', views.mylogin, name='login'),
     path('management', views.management, name='management'),
 ]
--- club/views.py
+++ club/views.py
@@ -10,15 +10,23 @@ def index(request):
     return render(request, 'club/index.html')
 
 
+available_players = ['a', 'a', 'b', 'a', 'a', 'b', 'a', 'a', 'b', 'a', 'a', 'b']
+blues = []
+whites = []
+
 def members(request):
     league_table = Member.objects.all().order_by('-points')
     next_fixture = Match.objects.all().order_by('-match_date')[0]
     all_matches = Match.objects.all().order_by('-match_date')
     past_results = all_matches[1:]
+    registrations_open = True
+    if len(available_players) > 11:
+        registrations_open = False
     context = {
         'league_table': league_table,
         'next_fixture': next_fixture,
-        'past_results': past_results
+        'past_results': past_results,
+        'registrations_open': registrations_open
     }
     return render(request, 'club/members.html', context)
 
@@ -56,44 +64,42 @@ def thankyou(request):
     return render(request, 'club/thankyou.html')
 
 
-def mylogin(request):
-    if request.method == 'POST':
-        form = LogInForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            approved_members = Member.objects.all().filter(is_approved=True)
-            login_attempts = 0
-            while login_attempts < 3:
-                for member in approved_members:
-                    if member['username'] == username:
-                        member_logging_in = member
-                        if member_logging_in['password'] == password:
-                            member_logging_in['is_logged_in'] == True
-                        else: 
-                            print('incorrect password')
-                            login_attempts += 1
-                            # return render(request, 'club/incorrect_password.html')
-                    else:
-                        print('no member with that username')
-                        # return render(request, 'club/no_user.html')
+# def mylogin(request):
+#     if request.method == 'POST':
+#         form = LogInForm(request.POST)
+#         if form.is_valid():
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password']
+#             approved_members = Member.objects.all().filter(is_approved=True)
+#             while password_attempts < 3:
+#             for member in approved_members:
+#                 if member['username'] == username:
+#                     member_logging_in = member
+#                     password_attempts = 0
+#                     if member_logging_in['password'] == password:
+#                         member_logging_in['is_logged_in'] == True
+#                         return 
+#                     else: 
+#                         print('incorrect password')
+#                         password_attempts += 1
+#                         # return render(request, 'club/incorrect_password.html')
+#                 else:
+#                     print('no member with that username')
+#                     # return render(request, 'club/no_user.html')
             
-    form = LogInForm()
-    return render(request, 'club/login.html', {
-        'form': form
-    })
+#     form = LogInForm()
+#     return render(request, 'club/login.html', {
+#         'form': form
+#     })
 
 
-available_players = []
-blues = []
-whites = []
+
 
 def book_match_place(player):
     if len(available_players) < 12:
         player['is_in_team'] = True
         available_players.append(player)
-        print('Your place on the team has been booked')
-        
+        print('Your place on the team has been booked')      
     else:
         reserves.append(player)
         print('Unfortunately there is no room on the team')

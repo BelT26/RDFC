2085c0eef916a5c4fccd7e77b656245fcd83f516
BelT26 <helen.taylor@hotmail.it>
2022-01-11 07:34:55 +0000

    import reverse and messages, remove original signup and login views and add docstrings to views.py

--- club/urls.py
+++ club/urls.py
@@ -4,9 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('members', views.members, name='members'),
-    #path('signup', views.mysignup, name='signup'),
     path('thankyou', views.thankyou, name='thankyou'),
-    #path('login', views.mylogin, name='login'),
     path('management', views.management, name='management'),
     path('applications', views.applications, name='applications'),
     path('result', views.add_result, name='result'),
--- club/views.py
+++ club/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 from .models import ClubMember, Match
-#from .forms import SignUpForm, LogInForm
+
 
 # Create your views here.
 
 
 def index(request):
+    """returns a view of the home page"""
     return render(request, 'club/index.html')
 
 
@@ -15,7 +17,12 @@ reserves = []
 blues = []
 whites = []
 
+
 def members(request):
+    """only accessible if a user is logged in.
+    returns a view containing details of team and playerscores,
+    the next match fixture and a booking form with which the
+    club member can book a place in the next match"""
     league_table = ClubMember.objects.all().order_by('-points')
     next_fixture = Match.objects.all().order_by('-match_date')[0]
     all_matches = Match.objects.all().order_by('-match_date')
@@ -33,94 +40,49 @@ def members(request):
 
 
 def management(request):
+    """returns a list of management functions"""
     return render(request, 'club/management.html')
 
 
 def applications(request):
+    """ returns a template showing any pending applications and
+    gives the manager the opportunity to approve them """
     return render(request, 'club/applications.html')
 
 
 def add_fixture(request):
+    """returns a form in which the manager can add, remove
+    or modify a match fixture"""
     return render(request, 'club/fixture.html')
 
 
 def add_result(request):
+    """returns a form in which the manager can add, remove
+    or modify a match result"""
     return render(request, 'club/result.html')
 
 
 def registrations(request):
+    """returns a form in which the manager can open or
+    or close registrations for the next match"""
     return render(request, 'club/registrations.html')
 
 
-# def mysignup(request):
-#     if request.method == 'POST':
-#         form = SignUpForm(request.POST)
-#         if form.is_valid():
-#             member = Member(
-#                 first_name=form.cleaned_data['first_name'],
-#                 last_name=form.cleaned_data['last_name'],
-#                 username=form.cleaned_data['username'],
-#                 email=form.cleaned_data['email'],
-#                 password=form.cleaned_data['password']
-#             )
-#             member.save()
-#             return HttpResponseRedirect('/thankyou')
-#     else:        
-#         form = SignUpForm()
-        
-#     return render(request, 'club/signup.html', {
-#         'form': form
-#     })
-
-
-def thankyou(request):
-    return render(request, 'club/thankyou.html')
-
-
-# def mylogin(request):
-#     if request.method == 'POST':
-#         form = LogInForm(request.POST)
-#         if form.is_valid():
-#             username = form.cleaned_data['username']
-#             password = form.cleaned_data['password']
-#             approved_members = Member.objects.all().filter(is_approved=True)
-#             while password_attempts < 3:
-#             for member in approved_members:
-#                 if member['username'] == username:
-#                     member_logging_in = member
-#                     password_attempts_remaining = 3
-#                     while password_attempts > 0
-#                     if member_logging_in['password'] == password:
-#                           member_logging_in['is_logged_in'] == True
-#                           messages.success(request, 'You have successfully logged in') 
-#                           return HttpResponseRedirect(reverse('members')
-#                     else: 
-#                         password_attempts_remaining -= 1
-                             
-#                         messages.error(request, f"Incorrect password. {password_attempts_remaining} attempts remaining )
-#                 else:
-#                     messages.error(request, 'Sorry no member is registered with that username')
-#                     form = LogInForm()
-#                     
-            
-#     form = LogInForm()
-#     return render(request, 'club/login.html', {
-#         'form': form
-#     })
-
-
-
-
 def book_match_place(request):
+    """allows club members to book a place on the team
+    for the next match. If teams are full the player is
+    placed on a reserve list"""
     player = None
     if request.user.is_authenticated():
         player = request.user
-        if len(available_players) <= 12:  
+        if len(available_players) <= 12:
             player['is_in_team'] = True
             available_players.append(player)
-            messages.success(request, 'You have been allocated a place in the next match!')
+            messages.success(request, 'You have been allocated a place in '
+                                      'the next match!')
         else:
             reserves.append(player)
-            messages.warning(request, 'Unfortunately there is no room on the team')
-            messages.warning(request, f'You are number {len(reserves)} on the reserve list')
+            messages.warning(request, 'Unfortunately there is no room on '
+                                      'the team')
+            messages.warning(request, f'You are reseve number {len(reserves)}.')
     return HttpResponseRedirect(reverse('members'))

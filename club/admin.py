3c263072e3bce9b482168cf9602b982034449eb3
BelT26 <helen.taylor@hotmail.it>
2022-01-11 06:13:39 +0000

    add contact details. add dropdown menu and template tags for home page

--- club/admin.py
+++ club/admin.py
@@ -8,7 +8,7 @@ class ClubMemberAdmin(admin.ModelAdmin):
 
     list_filter = ('is_approved', 'is_available')
     list_display = ('first_name', 'last_name', 'points', 'is_available')
-    search_fields = ['first_name', 'last_name']
+    search_fields = ['first_name', 'last_name','username']
     actions = ['approve_member']
 
     def approve_member(self, request, queryset):

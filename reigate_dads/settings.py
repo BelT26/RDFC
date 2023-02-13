0ed780b3f12ed78c425535e081ce2ec3f85f8d74
BelT26 <helen.taylor@hotmail.it>
2023-02-13 13:38:29 +0000

    fix indentation in settings.py

--- reigate_dads/settings.py
+++ reigate_dads/settings.py
@@ -35,9 +35,9 @@ DEBUG = False
 ALLOWED_HOSTS = ["reigate-dads.herokuapp.com", "localhost"]
 
 # Add Render.com URL to allowed hosts
- RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
- if RENDER_EXTERNAL_HOSTNAME:
-   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
